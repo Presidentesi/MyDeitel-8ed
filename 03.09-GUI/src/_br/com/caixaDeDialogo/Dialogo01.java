08898ba7d749c9d9a88c60318bf5064b074f4cc9
Presidentesi <william.cavalcante.ufpa@gmail.com>
2016-08-04 18:17:10 -0300

    Commit classe

---
+++ 03.09-GUI/src/_br/com/caixaDeDialogo/Dialogo01.java
@@ -0,0 +1,15 @@
+
+package _br.com.caixaDeDialogo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author William Cavalcante
+ */
+public class Dialogo01 {
+    public static void main (String[] args) {
+        JOptionPane.showMessageDialog(null, "Mensagem\nde\ndialogo!");
+        JOptionPane.showMessageDialog(null, "Mensagem!");
+    }  
+}
